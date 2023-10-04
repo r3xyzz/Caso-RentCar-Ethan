86b30c70b1dc8d1f4c29d25429b00b834f6cb8c9
Ethan Silvestre Badia <127980069+r3xyzz@users.noreply.github.com>
2023-10-03 23:35:55 -0300

    Add files via upload

---
+++ AlmacenVehiculos/src/almacenvehiculos/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package almacenvehiculos;
+
+import java.util.List;
+/**
+ *
+ * @author r3xzz
+ */
+// Clase Main.java
+
+public class Main {
+    public static void main(String[] args) {
+        AlmacenVehiculos almacen = new AlmacenVehiculos();
+
+        VehiculoCarga vehiculoCarga = new VehiculoCarga("ABC123", 100, 10, 5000);
+        VehiculoPasajeros vehiculoPasajeros = new VehiculoPasajeros("XYZ456", 50, 5, 4);
+
+        almacen.agregarVehiculo(vehiculoCarga);
+        almacen.agregarVehiculo(vehiculoPasajeros);
+
+        List<Vehiculo> listaVehiculos = almacen.obtenerVehiculos();
+        for (Vehiculo vehiculo : listaVehiculos) {
+            vehiculo.mostrarBoleta();
+        }
+
+        int cantidadVehiculosMayor7Dias = almacen.contarVehiculosArriendoMayor7Dias();
+        System.out.println("Cantidad de vehículos con arriendo mayor o igual a 7 días: " + cantidadVehiculosMayor7Dias);
+    }
+}
